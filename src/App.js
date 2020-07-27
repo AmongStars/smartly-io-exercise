ec97084b76407a1bbe2a67216ab8c864b72454ec
zigzag8881 <ninabarnabishvili.viidakko@gmail.com>
2020-07-27 22:50:47 +0300

    Display five random facts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = "https://cat-fact.herokuapp.com/";
+const DEFAULT_QUERY = "facts";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      items: [],
+      error: null,
+    };
+  }
+
+  fetchFacts() {
+    fetch(API + DEFAULT_QUERY)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          items: data["all"],
+          isLoading: false,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ error, isLoading: false });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchFacts();
+  }
+
+  getRowData(item) {
+    const id = item._id;
+    if (!id) {
+      return null;
+    }
+
+    const name = item?.user?.name;
+    if (!name) {
+      return null;
+    }
+
+    const { first, last } = name;
+    if (!first && !last) {
+      return null;
+    }
+
+    const { text } = item;
+    if (!text) {
+      return null;
+    }
+
+    const user = `${first} ${last}`.trim();
+
+    return {
+      id,
+      user,
+      text,
+    };
+  }
+
+  getRandomElements(items, size) {
+    const randomElements = [];
+    for (let i = 0; i < size; i++) {
+      randomElements.push(items[Math.floor(Math.random() * items.length)]);
+    }
+    return randomElements;
+  }
+
+  render() {
+    const { isLoading, items, error } = this.state;
+    return (
+      <React.Fragment>
+        <h1>Random facts about cats</h1>
+        {error ? <p>{error.message}</p> : null}
+        {!isLoading ? (
+          this.getRandomElements(items, 5).map((item) => {
+            const rowData = this.getRowData(item);
+            return rowData ? (
+              <div key={rowData.id}>
+                <p>User: {rowData.user}</p>
+                <p>Fact: {rowData.text}</p>
+                <hr />
+              </div>
+            ) : null;
+          })
+        ) : (
+          <h3>Loading...</h3>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
